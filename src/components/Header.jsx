1a7fd3f34e754e072a19548183cf93949b0b17cf
unknown <chuan661999@gmail.com>
2024-09-04 12:49:17 +0700

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Login from './Login';
 
 import LOGO from '../img/logo.png'
-import LOGO from '../img/daotruyen_cheat.png'
+import CHEAT from '../img/daotruyen_cheat.png'
 
 import { IoSearch, IoMoonOutline, IoPeopleOutline, IoCloudUploadOutline } from "react-icons/io5";
 import { BiCategory } from "react-icons/bi";
