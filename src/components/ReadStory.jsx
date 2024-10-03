a6e01e571f87c1684dc28ded2930006ec77b8807
Thanh Binh <buithanhbinh.dn12@gmail.com>
2024-10-03 23:38:05 +0700

    add user-select

--- src/components/ReadStory.jsx
+++ src/components/ReadStory.jsx
@@ -16,14 +16,13 @@ function ReadStory() {
     const { chaptersData } = useStoriesContext()
 
     const storyNameRef = useRef(JSON.parse(localStorage.getItem("story")))
-
     
     const [chapterData, setChapterData] = useState([])
     const [seletedChapter, setSelectedChapter] = useState(id)
     const { content: contentChapters } = chaptersData
-    const [chaptersOption, setChaptersOption] = useState(contentChapters)
+    // const [chaptersOption, setChaptersOption] = useState(contentChapters)
     
-    const { chapter, paragraphs, chapterPreviousId, chapterNextId, first } = chapterData
+    const { chapter, paragraphs, chapterPreviousId, chapterNextId, first, last } = chapterData
 
     useEffect(() => { 
         fetchChapter(`${chapter_url}?chapterId=${id}`)
@@ -69,45 +68,49 @@ function ReadStory() {
         }
     }
 
-    console.log('chapterData: ',  chapterData)
-    console.log('selected chapter', seletedChapter)
-    console.log('id: ', id)
+    const handleMouseDown = e => {
+        e.preventDefault()
+    }
+
+    // console.log('chaptersOption ', chaptersOption)
 
     return (
-        <main className="px-0 py-6 lg:p-6 bg-[#f7f7f7f7]">
-            <div className="lg:mx-16 px-4 md:px-6">
-                <div>
-                    <ul className="flex items-center bg-white mb-5 py-1 pr-1">
-                        <li className='relative -mb-2 text-white'>
-                            <Link className='p-2 inline-block bg-main after:absolute after:left-full after:top-0 after:content-[""] after:border-[16px] after:border-solid after:border-transparent after:border-l-main' to={"/"}>
-                                <i><IoHome/></i>
-                            </Link>
-                        </li>
-                        <li className="ml-8">
-                            <Link to={`/story-detail/${storyNameRef.current && storyNameRef.current[0]}`}>
-                                {storyNameRef.current && storyNameRef.current[1]}
-                            </Link>
-                        </li>
-                        <li className="px-2"><i><IoIosArrowForward/></i></li>
-                        <li>Chương {chapterData.chapter && chapterData.chapter.chapterNumber}</li>
-                    </ul>
+        <>
+            <main onMouseDown={handleMouseDown} className="px-0 py-6 lg:p-6 bg-[#f7f7f7f7]">
+                <div className="lg:mx-16 px-4 md:px-6">
                     <div>
-                        <div className="p-3 bg-slate-300 border-l-2 border-solid border-main">
-                            <h1 className="text-2xl font-medium mb-2">Chương {chapterData.chapter && chapterData.chapter.chapterNumber}{chapterData.chapter && chapterData.chapter.title ? `: ${chapterData.chapter.title}` : null}</h1>
-                            <p>Theo dõi <span className="font-bold text-main">Đảo truyện</span> trên facebook để trò chuyện, giao lưu và xem lịch cập nhật truyện mới nhất...</p>
+                        <ul className="flex items-center bg-white mb-5 py-1 pr-1">
+                            <li className='relative -mb-2 text-white'>
+                                <Link className='p-2 inline-block bg-main after:absolute after:left-full after:top-0 after:content-[""] after:border-[16px] after:border-solid after:border-transparent after:border-l-main' to={"/"}>
+                                    <i><IoHome/></i>
+                                </Link>
+                            </li>
+                            <li className="ml-8">
+                                <Link to={`/story-detail/${storyNameRef.current && storyNameRef.current[0]}`}>
+                                    {storyNameRef.current && storyNameRef.current[1]}
+                                </Link>
+                            </li>
+                            <li className="px-2"><i><IoIosArrowForward/></i></li>
+                            <li>Chương {chapterData.chapter && chapterData.chapter.chapterNumber}</li>
+                        </ul>
+                        <div>
+                            <div className="p-3 bg-slate-300 border-l-2 border-solid border-main">
+                                <h1 className="text-2xl font-medium mb-2">Chương {chapterData.chapter && chapterData.chapter.chapterNumber}{chapterData.chapter && chapterData.chapter.title ? `: ${chapterData.chapter.title}` : null}</h1>
+                                <p>Theo dõi <span className="font-bold text-main">Đảo truyện</span> trên facebook để trò chuyện, giao lưu và xem lịch cập nhật truyện mới nhất...</p>
+                            </div>
+                        </div>
+                        <div className="mt-6 md:p-4 select-none text-xl read_str_content">
+                            {paragraphs && paragraphs.filter((paragraph) => paragraph !== '\r').map((paragraph, i) => <p key={i}>{paragraph}</p>)}
                         </div>
                     </div>
-                    <div className="mt-6 md:p-4">
-                        {paragraphs && paragraphs.map((paragraph, i) => <p key={i} className="text-xl mb-4">{paragraph}</p>)}
-                    </div>
                 </div>
-            </div>
 
-            <div className="fixed right-0 bottom-0 left-0 z-50 h-16 md:px-10 md:-ml-10">
+            </main>
+            <div className="fixed right-0 bottom-0 left-0 z-[100] h-16 md:px-10 md:-ml-10">
                 <div className="flex justify-center items-center h-full">
                     <button 
-                        className={`px-3 py-3 text-white bg-main rounded-l-3xl ${chapterPreviousId === 0 ? 'opacity-40' : ''}`}
-                        disabled={chapterPreviousId === 0 ? true : false}
+                        className={`px-3 py-3 text-white bg-main rounded-l-3xl ${chapterPreviousId === 0 ? 'opacity-40' : '' } hover:opacity-90`}
+                        disabled={first}
                         onClick={handlePrevChapter}
                     >Chương trước</button>
                     <select 
@@ -118,18 +121,18 @@ function ReadStory() {
                         // onChange={(e) => setSelectedChapter(e.target.value)}
                         onChange={handleSelectedChapter}
                     >
-                        {chaptersOption && chaptersOption.map((itemChapter, i) => 
+                        {contentChapters && contentChapters.map((itemChapter, i) => 
                             <option key={i} value={itemChapter.id}>Chương {itemChapter.chapterNumber}</option>
                         )}
                     </select>
                     <button 
-                        className={`px-3 py-3 text-white bg-main rounded-r-3xl ${chapterNextId === 0 ? 'opacity-40' : ''}`}
-                        disabled={chapterNextId === 0 ? true : false}
+                        className={`px-3 py-3 text-white bg-main rounded-r-3xl ${chapterNextId === 0 ? 'opacity-40' : ''} hover:opacity-90`}
+                        disabled={last}
                         onClick={handleNextChapter}
                     >Chương sau</button>
                 </div>
             </div>
-        </main>
+        </>
     )
 }
 
